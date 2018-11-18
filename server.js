23e0c5b00384d4c2fe5560198ee2021bd2de9886
Irina O'Hara <irina.v.ohara@gmail.com>
2018-11-17 21:24:02 -0700

    deploy express app

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	console.log("Received a request for /");
+	res.write("This is the root.");
+	res.end();
+});
+
+app.get("/home", function(req, res){
+	console.log("Received a request for a home page");
+	res.render("home");
+});
+
+app.listen(5000, function(){
+	console.log("Listening on port 5000");
+});
